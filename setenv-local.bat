986bbfd2b2ef66fa5c2104f1af4a580ea176e83e
Jignesh Bhalabhai Prajapati <jignesh.prajapati@tavant.com>
2020-07-06 10:29:10 +0530

    Start of the shelving work

---
+++ setenv-local.bat
@@ -0,0 +1,9 @@
+set JAVA_HOME=C:\PROGRA~1\Java\jdk1.8.0_161
+set DOCKER_HOME=C:\PROGRA~1\Docker\Docker
+set DOCKER_BIN=C:\PROGRA~1\Docker\Docker\resources\bin
+set DOCKER_CMD=C:\PROGRA~1\Docker\Docker\resources\bin\docker
+set KAFKA_HOME=D:\softwares\kafka\kafka_2.11-2.2.0
+set WIN_CMD=C:\Windows\System32
+set GIT_HOME=C:\PROGRA~1\Git
+
+set PATH=%PATH%;%JAVA_HOME%\bin;%DOCKER_HOME%\resources\bin;%KAFKA_HOME%\bin\windows;%WIN_CMD%\wbem;%WIN_CMD%;%GIT_HOME%\bin
